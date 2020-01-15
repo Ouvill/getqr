b31a38931a157551d896ffe09e002070974cea36
Ouvill <contact@ouvill.net>
2020-01-15 23:47:13 +0900

    error 表示追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import logger from "morgan";
+import createError from "http-errors";
 
 import indexRouter from "./routes/index";
 
@@ -8,10 +10,30 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routing
 app.get("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: any, req: any, res: any, next: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 app.listen(3000, () => {
   console.log("server run: http://localhost:3000");
