6e05cf585c6525252ba354e29768b4554e2f200d
Ouvill <contact@ouvill.net>
2020-01-15 23:02:49 +0900

    qr code の生成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import QRCode from "qrcode";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/", async (req, res) => {
+  try {
+    const query = req.query;
+    const string = query.string || query.url;
+    if (string) {
+      const url = await QRCode.toDataURL(string);
+      res.send(url);
+    } else {
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(3000, () => {
